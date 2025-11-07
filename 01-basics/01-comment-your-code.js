efacb57eff9ce1daaf50bac415926fd57a932f5c
ramialtayan <rami.altayan1@gmail.com>
2025-11-07 10:21:43 -0800

    Update 01-comment-your-code.js

--- 01-basics/01-comment-your-code.js
+++ 01-basics/01-comment-your-code.js
@@ -1,1 +1,14 @@
+//  Lesson 01: Comment Your JavaScript Code
+//  FCC Link: https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/comment-your-javascript-code
 
+// This is a single-line comment
+
+/* 
+  This is a multi-line comment
+  spanning more than one line 
+*/
+
+//  Notes:
+// - Comments are ignored by the JavaScript engine.
+// - Use comments to explain *why* code exists, not what is obvious.
+// - Good commenting improves readability for future developers (including yourself!).
